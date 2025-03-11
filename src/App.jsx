19b18629ef934c8a8d404cab65c001ac22633dc2
Irving Cruz Chávez <ic3386941@gmail.com>
2025-03-11 17:50:14 -0600

    Final de la clase Martes 11/03/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Bar from './Components/Search_Bar.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Bar></Bar>
     </>
   )
 }
---
+++ src/Components/Search_Bar.jsx
@@ -0,0 +1,13 @@
+import 
+function Bar() {
+  return (
+    <div className="form">
+      <div className="container primary">
+        <label htmlFor="">Hola</label>
+        <input className="form-control" type="text" />
+      </div>
+    </div>
+  );
+}
+
+export default Bar;
