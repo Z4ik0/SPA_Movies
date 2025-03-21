75dacae1064b462a9342bacb45fb042bc736f29f
Irving Cruz Chávez <ic3386941@gmail.com>
2025-03-21 14:35:18 -0600

    Fusion de cambios de github a local

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 import Bar from './Components/Search_Bar.jsx'
 import Card_Component from './Components/Card_Component.jsx'
 
 
 function App() {
+
   return (
     <>
       <Card_Component></Card_Component>
--- src/Components/Search_Bar.jsx
+++ src/Components/Search_Bar.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import Card_Component from "./Card_Component";
+import "./Search_bar.css";
+import { useFetch } from "./hooks/useFetch";
+
 function Bar() {
+  const [search, setSearch] = useState("");
+
+  const API_URL = import.meta.env.VITE_API_URL;
+  const API_ACCESS_TOKEN = import.meta.env.VITE_API_ACCESS_TOKEN;
+
+  const { data, isPending } = useFetch(API_URL, API_ACCESS_TOKEN);
+
+  // ! Sigue el input
+  const searcher = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // ! filtro
+  let pelis = [];
+  if (!search) {
+    pelis = data;
+  } else {
+    pelis = data.filter((dato) =>
+      dato.name.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+  }
+
   return (
-    <div className="form">
-      <div className="container primary">
-        <label htmlFor="">Hola</label>
-        <input className="form-control" type="text" />
+    <>
+      <div className="form">
+        <div className="container">
+          <label className="form-label text" htmlFor="Searchbar">
+            Encuentra tu pelicula
+          </label>
+          <input
+            value={search}
+            onChange={searcher}
+            id="Searchbar"
+            className="form-control input"
+            placeholder="Buscar.."
+            type="text"
+          />
+        </div>
       </div>
-    </div>
+      <Card_Component data={data} isPending={isPending}></Card_Component>
+    </>
   );
 }
 
