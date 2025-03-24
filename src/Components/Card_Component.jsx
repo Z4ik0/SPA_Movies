24917e6ef13c52d9f7aab7ce5c2ccfb88ac8a905
ZeVa16 <arturo1414zv@hotmail.com>
2025-03-24 11:59:35 -0600

    FIX cambio en css al Card

--- src/Components/Card_Component.jsx
+++ src/Components/Card_Component.jsx
@@ -5,26 +5,26 @@ import "./Card.css"
 
 // const API_URL = import.meta.env.VITE_API_URL
 // const API_ACCESS_TOKEN = import.meta.env.VITE_API_ACCESS_TOKEN
- const IMAGE_PATH = import.meta.env.VITE_IMG_BASIC_PATH
+const IMAGE_PATH = import.meta.env.VITE_IMG_BASIC_PATH
 
 function Card_Component({ data, isPending }) {
 
     // console.log(API_URL, API_ACCESS_TOKEN)
     // const { data, isPending } = useFetch(API_URL, API_ACCESS_TOKEN);
-    
+
     return (
         <div className='container'>
             <div className='row gap-4  justify-content-md-center'>
                 {isPending ? <Loading></Loading> : ''}
                 {data != null ? data.map((movie) => {
                     return (
-                        <div className="card col-3"  key={movie.id} style={{ overflow: 'hidden' }}>
+                        <div className="card col-3" key={movie.id} style={{ overflow: 'hidden' }}>
                             <div>
                                 <img className='card-img-top' style={{ maxHeight: '100%' }} src={`${IMAGE_PATH}/${movie.backdrop_path}`} alt={movie.original_title + " image"} />
                             </div>
-                            <div className="card-body">
-                                <h2 className="card-title">{movie.original_title}</h2>
-                                <p className="card-text" style={{ textAlign: 'justify' }}>{movie.overview}</p>
+                            <div className="card-body d-flex">
+                                <h3 className="card-title">{movie.original_title}</h3>
+                                <p className="card-text scroll" style={{textAlign: 'justify'}}>{movie.overview}</p>
                             </div>
                         </div>
                     )
